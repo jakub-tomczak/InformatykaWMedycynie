07ab0fed90f5290eb02de11b9832bf563df9db59
Jakub Tomczak <jakub.tomczak@live.com>
2018-03-18 10:38:45 +0100

    Fix drawing straight line

--- TomografKomputerowy/line_drawer.py
+++ TomografKomputerowy/line_drawer.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 def bresenham(image, start, end):
     x1,y1 = start
     x2,y2 = end
@@ -19,7 +20,7 @@ def bresenham(image, start, end):
         draw_straight_line(image, start, end)
     else:
         if dx > dy:
-            #os wiodaca OX
+            #OX priority
             for i in range(0, dx):
                 x1 = x1 + xi
                 e = e - dy
@@ -28,7 +29,7 @@ def bresenham(image, start, end):
                     e = e + dx
                 image[y1, x1] = 1
         else:
-            #os wiodaca OY
+            #OY priority
             for i in range(0, dy):
                 y1 = y1 + yi
                 e = e - dx
@@ -39,15 +40,18 @@ def bresenham(image, start, end):
 
 def draw_straight_line(image, start, end):
     if start[0] == end[0]:
-        image[start[0],:] = 1
-        print("all",start[0], "y:", start[1], end[1])
+        image[:,start[0]] = 1
     if end[1] == start[1]:
-        image[:,start[1]] = 1
-        print("all", start[1], "x:", start[0], end[0])
+        image[start[1],:] = 1
 
 def calculatePosition(angle, radius, circle_middle = (0,0)):
     rad = angle / 180 * np.pi
     return (circle_middle[0] + (int)(radius*np.cos(rad)), circle_middle[1] - (int)(radius*np.sin(rad)))
+
+'''Calculates position x1, y1 and changes these values according to the available space'''
+def calculatePositionSafe(angle, diameter, circle_middle = (0,0)):
+    x1, y1 = calculatePosition(angle, (int)(diameter / 2) - 1, circle_middle)
+    return check_borders((x1, y1), diameter)
 
 def check_value(val, size):
     if val <= 0:
@@ -55,18 +59,21 @@ def check_value(val, size):
     if val >=size:
         val = size -1
     return val
+
 def check_borders(point, size):
     return (check_value(point[0], size),check_value(point[1], size))
-
-
 
 def calculate_line(image, angle):
     size = len(image)
     radius = (int)(size / 2) - 1
     center = (radius, radius)
-    x1, y1 = calculatePosition(angle, radius, center)
-    x1, y1 = check_borders((x1,y1), size)
+    calculate_line(image, angle, size, center)
 
-    x2 = size - x1
-    y2 = size - y1
+def calculate_line(image, angle, diameter, center):
+    radius = (int)(diameter / 2) - 1
+    x1, y1 = calculatePosition(angle, radius, center)
+    x1, y1 = check_borders((x1,y1), diameter)
+
+    x2 = diameter - x1
+    y2 = diameter - y1
     bresenham(image, (x1, y1), (x2, y2))
