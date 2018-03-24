4a0c5ea204beff7475bd3f92fc60239c2a79122e
Jakub Tomczak <jakub.tomczak@live.com>
2018-03-24 12:36:36 +0100

    Reconstruct image from sinogram.

--- TomografKomputerowy/line_drawer.py
+++ TomografKomputerowy/line_drawer.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-#returns sum of pixels values that Bresenham's alg went through
-def bresenham(oryg_image,image, start, end, sinogram_arr):
+#returns sum of pixels values that Bresenham's alg went through or reconstructs image along path
+def bresenham(start, end, oryg_image = None,image = None, sinogram_value = None, reconstruction_image = None):
     value = 255
     x1,y1 = start
     x2,y2 = end
@@ -17,11 +17,22 @@ def bresenham(oryg_image,image, start, end, sinogram_arr):
     dy = np.abs(y2 - y1)
     e = dx / 2
     sum = 0
+    count = 0
     if dx == 0 or dy == 0:
-        sum += draw_straight_line(image, start, end)
+        if reconstruction_image is None:
+            draw_straight_line(image, start, end)
+            sum += np.sum(oryg_image[y1, :]) if dx == 0 else np.sum(oryg_image[:, x1])
+            count += len(oryg_image)
+        else:
+            reconstruct_straight_line(reconstruction_image, sinogram_value, start, end)
     else:
-        image[y1, x1] = value
-        sum += oryg_image[y1, x1]
+        if reconstruction_image is None:
+            image[y1, x1] = value
+            sum += oryg_image[y1, x1]
+            count += 1
+        else:
+            reconstruction_image[y1, x1] += sinogram_value
+
         if dx > dy:
             #OX priority
             for i in range(0, dx):
@@ -30,8 +41,13 @@ def bresenham(oryg_image,image, start, end, sinogram_arr):
                 if e < 0:
                     y1 = y1 + yi
                     e = e + dx
-                image[y1, x1] = value
-                sum += oryg_image[y1, x1]
+                if reconstruction_image is None:
+                    image[y1, x1] = value
+                    sum += oryg_image[y1, x1]
+                    count += 1
+                else:
+                    reconstruction_image[y1, x1] += sinogram_value
+
         else:
             #OY priority
             for i in range(0, dy):
@@ -40,18 +56,28 @@ def bresenham(oryg_image,image, start, end, sinogram_arr):
                 if e < 0:
                     x1 = x1 + xi
                     e = e + dy
-                image[y1, x1] = value
-                sum += oryg_image[y1, x1]
-    return sum
+                if reconstruction_image is None:
+                    image[y1, x1] = value
+                    sum += oryg_image[y1, x1]
+                    count += 1
+                else:
+                    reconstruction_image[y1, x1] += sinogram_value
+
+    return (sum // count if reconstruction_image is None else None)
+
+def reconstruct_straight_line(image, value, start, end):
+    if start[0] == end[0]:
+        image[:, start[0]] += value
+    if end[1] == start[1]:
+        image[start[1], :] += value
 
 def draw_straight_line(image, start, end):
-    print("drawing straight line")
+    #print("drawing straight line")
     if start[0] == end[0]:
         image[:,start[0]] = 1
     if end[1] == start[1]:
         image[start[1],:] = 1
-    #image is square
-    return len(image)
+
 
 def calculatePosition(angle, radius, circle_middle = (0,0)):
     rad = angle / 180 * np.pi
@@ -59,7 +85,7 @@ def calculatePosition(angle, radius, circle_middle = (0,0)):
 
 '''Calculates position x1, y1 and changes these values according to the available space'''
 def calculatePositionSafe(angle, diameter, circle_middle = (0,0)):
-    x1, y1 = calculatePosition(angle, (int)(diameter / 2) - 1, circle_middle)
+    x1, y1 = calculatePosition(angle, diameter // 2 - 1, circle_middle)
     return check_borders((x1, y1), diameter)
 
 def check_value(val, size):
@@ -72,24 +98,24 @@ def check_value(val, size):
 def check_borders(point, size):
     return (check_value(point[0], size),check_value(point[1], size))
 
+def reconstruct_line(sinogram_value, reconstruction_image, start_point, end_point):
+    bresenham(sinogram_value=sinogram_value, reconstruction_image=reconstruction_image, start=start_point, end=end_point)
+
 #draws line between start_point and end_point or draws a diameter using the angle
-def draw_line(oryg_image, image,  sinogram_arr,angle = None, diameter = None, center = None, start_point = None, end_point = None):
+def draw_line(oryg_image, image, angle = None, diameter = None, center = None, start_point = None, end_point = None):
     if start_point != None and end_point != None:
-        return bresenham(oryg_image, image, start_point, end_point, sinogram_arr)
+        return bresenham(start_point, end_point, oryg_image=oryg_image, image=image)
 
     if diameter == None:
         diameter = len(image)
 
-    radius = (int)(diameter / 2) - 1
+    radius = diameter // 2 - 1
 
     if center == None:
         center = (radius, radius)
 
-    x1, y1 = calculatePosition(angle, radius, center)
-    x1, y1 = check_borders((x1,y1), diameter)
-
-    #print(x1,y1)
+    x1, y1 = calculatePositionSafe(angle, diameter, center)
 
     x2 = diameter - x1
     y2 = diameter - y1
-    return bresenham(oryg_image, image, (x1, y1), (x2, y2), sinogram_arr)
+    return bresenham((x1, y1), (x2, y2), oryg_image=oryg_image, image=image)
--- TomografKomputerowy/main.py
+++ TomografKomputerowy/main.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from line_drawer import \
-    bresenham, draw_line, calculatePosition, check_borders, calculatePositionSafe
+    bresenham, draw_line, calculatePosition, check_borders, calculatePositionSafe, reconstruct_line
 
 
 def radon_example(image):
@@ -31,116 +31,117 @@ def plot_image(image):
     plt.subplot(1, 1, 1), plt.imshow(image, cmap='gray')
     plt.show()
 
-def draw_rays_old(oryg_image, img, angle, n_detectors, sinogram_arr, emission_angle, diameter):
-    if n_detectors % 2 == 0:
-        n_detectors += 1
-    #Draw main line
-    if diameter == 0:
-        diameter = len(image)
-    big_diameter = diameter
-    big_radius = (int)(big_diameter / 2) - 1
-    big_center = (big_radius, big_radius)
-    start = calculatePositionSafe(angle, big_diameter, big_center)
-    draw_line(oryg_image, image, sinogram_arr, angle=angle, diameter=big_diameter, center=big_center)
+def inverse_radon(image, sinogram, diameter, angle, emission_angle, n_detectors):
+    angle_between_rays = emission_angle / (n_detectors - 1)
+    angles = np.arange(-emission_angle / 2, emission_angle / 2 + emission_angle / n_detectors, angle_between_rays)
 
-    x2 = big_diameter - start[0]
-    y2 = big_diameter - start[1]
+    radius = diameter // 2 - 1
+    center = (radius, radius)
+    start_point = calculatePositionSafe(angle, diameter, center)
 
-    small_diameter = (int)(big_diameter/2)
-    #get center of smaller circle
-    small_center = ( (int)((x2+big_center[0])/2), (int)((y2+big_center[1])/2) )
-    small_radius = (int)(small_diameter/2)-1
+    x = 0
+    for y in angles:
+        end_point = calculatePositionSafe(angle + y*2, diameter, center)
+        end_point = (diameter - end_point[0], diameter - end_point[1])
+        reconstruct_line(start_point=start_point, end_point=end_point, sinogram_value=sinogram[angle, x], reconstruction_image=image)
+        x+=1
+def normalize_image(image):
+    maxV = np.max(image)
+    return (image / maxV ) * 255
 
-    angle_between_detectors = 5
-    deviation = angle_between_detectors * (int)(n_detectors / 2)
-    for i in range(angle - deviation, angle + deviation + 1, angle_between_detectors):
-        if i == angle:
-            continue
-        end = calculatePosition(i, small_radius, small_center )
-        end = (2*small_center[0] - end[0], 2*small_center[1] - end[1])
-        end = check_borders(end, big_diameter)
-        print(start, end)
-        draw_line(oryg_image, image, sinogram_arr, start_point=start, end_point=end)
-
-def calculate_emission_ray(start_point, diameter, angle):
-    end_point = (diameter - start_point[0], diameter - start_point[1])
-    end_point = check_borders(end_point, diameter)
-    z = np.tan(angle*np.pi/180)*diameter
-    dx = np.sin(angle*np.pi/180)*z
-    dy = np.cos(angle*np.pi/180)*z
-    if angle < -24 or angle > 24:
-        print(angle, dx, dy, end_point)
-    x = (int)(end_point[0] + dx)
-    y = (int)(end_point[1] + dy)
-    return check_borders((x, y), diameter)
-
-def draw_rays_new(oryg_image, image, angle, n_detectors, sinogram_arr, emission_angle, diameter):
+def radon(oryg_image, image, angle, n_detectors, sinogram_arr, emission_angle, diameter):
     angle_between_rays = emission_angle / (n_detectors - 1)
     angles = np.arange(-emission_angle/2, emission_angle/2 + emission_angle/n_detectors , angle_between_rays)
 
     radius = diameter // 2 - 1
     center = (radius, radius)
-    main_ray_coordinates = calculatePositionSafe(angle, diameter, center)
+    start_point = calculatePositionSafe(angle, diameter, center)
 
     x = 0
     for i in angles:
-        start_point = main_ray_coordinates
         end_point = calculatePositionSafe(angle + i*2, diameter, center)
         end_point = (diameter - end_point[0], diameter - end_point[1])
-        sum = draw_line(oryg_image, image, image, sinogram_arr, start_point=start_point, end_point=end_point)
-        #print(i,x, sum)
-        #sinogram_arr[angle, x] = sum
+        sum = draw_line(oryg_image, image, start_point=start_point, end_point=end_point)
+        sinogram_arr[angle, x] = sum
         x += 1
 
-def draw_emitter(oryg_image,image, angle, n_detectors, sinogram_arr, emission_angle, diameter = 0, mode = 1):
-    if mode == -1:
-        draw_rays_old(oryg_image, image, angle, n_detectors, sinogram_arr, emission_angle, diameter)
-    else:
-        draw_rays_new(oryg_image, image, angle, n_detectors, sinogram_arr, emission_angle, diameter)
+def get_new_image_shape(old_image):
+    if old_image.shape[0] != old_image.shape[1]:
+        raise Exception('Image is not a square!')
+    #size is sqrt(2)*a -> diagonal of a square
+    size = (int)(np.sqrt(2) * max(old_image.shape[0], old_image.shape[1]) + 10)
+    return (size, size)
 
 
 #creates quare matrix that is sqrt(2)*size(image)+10 long
 def prepare_image(image):
-    if image.shape[0] != image.shape[1]:
-        raise Exception('Image is not a square!')
-    #size is sqrt(2)*a -> diagonal of a square
-    size = (int)(np.sqrt(2) * max(image.shape[0], image.shape[1]) + 10)
-    new_image = np.zeros((size, size))
+    size = get_new_image_shape(image)
+    new_image = np.zeros(size)
     dy = image.shape[0] // 2
     dx = image.shape[1] // 2
-    new_center = size // 2
+    new_center = size[0] // 2
     # put image into new_image
     new_image[new_center - dy: new_center + dy, new_center - dx: new_center + dx] = image
 
     return new_image
 
-def process(image):
-    new_image = prepare_image(image)
-    #keep original image matrix to get original values
-    oryg_image = np.array(new_image)
-    sinogram_arr = np.zeros((180, n_detectors))
 
-    #loop that draws rays
-    #for i in range(80, 100):
-    #    draw_emitter(oryg_image, new_image, i, n_detectors, sinogram_arr, emission_angle, diameter=len(new_image))
-    draw_emitter(oryg_image, new_image, 90, n_detectors, sinogram_arr, emission_angle, diameter=len(new_image))
-    plot_image(new_image)
+
+def display_status(num, all):
+    os.system('cls')
+    p = num * 100 // all
+    print("{status}>{spaces}{percent}%".format(status='-' * (p), spaces=(100 - p - 1) * ' ', percent=p))
+
+def process(image):
+    #new image
+    new_image_size = get_new_image_shape(image)
+
+    #keep original image matrix to get original values
+    oryg_image = prepare_image(image)
+    reconstructed = np.zeros(new_image_size)
+    sinogram_arr = np.zeros((radon_angle, n_detectors))
+
+    #create sinogram
+    print("Creating sinogram")
+    for i in range(0, 360):
+        rays_image = np.zeros(new_image_size)
+        display_status(i, radon_angle)
+        radon(oryg_image, rays_image, i, n_detectors, sinogram_arr, emission_angle, diameter=new_image_size[0])
+    sinogram_arr = normalize_image(sinogram_arr)
+
+
+    print('Reconstructing image')
+    #reconstruct image
+    for i in range(0, radon_angle):
+        display_status(i, radon_angle)
+        inverse_radon(reconstructed, sinogram_arr, diameter=new_image_size[0], angle=i, emission_angle=emission_angle,n_detectors=n_detectors)
+
+    return sinogram_arr, reconstructed
 
 def debug(image):
+    # im = discrete_radon_transform(image, 360)
+    # im = misc.imrotate(im, 90).astype('float64')
+
+    # plot_image(im)
+
      angle = 360
      im = np.zeros(image.shape)
+     l = len(image) // 4
+     im[l:2*l, l:2*l] = 255
+     im[l*2:l*3, l*2:l*3] = 255
+     return im
      sinogram_arr = np.zeros((angle, len(image)))
-     draw_rays_new(im, im, 0, 60, None, 20, len(image))
+     radon(im, im, 0, 60, None, 20, len(image))
      for i in range(0,angle):
-        rotation = misc.imrotate(im, i ).astype('float64')
+        rotation = misc.imrotate(im, i).astype('float64')
         result = np.multiply(rotation, image)
         #plot_image(result)
         #plot_image(rotation)
         res = sum(result)
         sinogram_arr[i:] = res
-     plot_image(sinogram_arr)
 
 
+radon_angle = 360
 if __name__ == "__main__":
     file = "photo.png"
     directory = os.getcwd() + "\\res\\"
@@ -148,13 +149,13 @@ if __name__ == "__main__":
     #   liczba detektorów
     n_detectors = 100
     #   rozpiętość kątowa
-    emission_angle = 50
+    emission_angle = 120
 
     image = misc.imread('{dir}{file}'.format(dir=directory, file=file), flatten=True).astype('float64')
-
-    #debug(image)
-
-    process(image)
-    # plt.subplot(1, 1, 1), plt.imshow(new_image, cmap='gray')
-    #plt.show()
+    #get image from debug source
+    #image = debug(image)
+    sinogram, reconstructed = process(image)
+    plot_image(reconstructed)
+    expected_sinogram = discrete_radon_transform(image, radon_angle)
+    plot_image(expected_sinogram)
 
